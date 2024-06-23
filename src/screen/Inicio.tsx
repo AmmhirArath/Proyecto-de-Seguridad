0a529e78fbbf458932612533904b24a69cec793a
ADRAXLIFE <miguel.sanchez6@utp.ac.pa>
2024-06-22 19:18:20 -0500

    ApreAsul
    Pantalla de infromacion para los adultos

--- src/screen/Inicio.tsx
+++ src/screen/Inicio.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Colores from '../constants/Colores';
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Colores from "../constants/Colores";
 
 const Inicio = () => {
   return (
@@ -8,18 +8,18 @@ const Inicio = () => {
       <Text style={styles.text}>Bienvenido a la pantalla de Inicio</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colores.fondo,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 24,
-    color: 'white',
+    color: "white",
   },
 });
 
