906dbdc4667fbd53ab7baf70b3a3a961d729efba
Ammhir Arath Carvajal <112657623+AmmhirArath@users.noreply.github.com>
2024-06-27 23:52:04 -0500

    Quitar notificaciones

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import AuthStack from './src/navigation/AuthStack';
-
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); 
+LogBox.ignoreAllLogs();
 
 const App = () => {
   const [fontsLoaded, fontError]  = useFonts({
