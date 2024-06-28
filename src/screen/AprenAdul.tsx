e04db9e471cef4b798d550eb381cdc9d250ae8f7
ADRAXLIFE <miguel.sanchez6@utp.ac.pa>
2024-06-28 13:17:54 -0500

    Por Terminar
    Por Terminar la pantalla de  informacion se le agrego el sistema de  audio

--- src/screen/AprenAdul.tsx
+++ src/screen/AprenAdul.tsx
@@ -1,25 +1,86 @@
-import * as React from "react";
-import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
-import Colores from "../constants/Colores";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+import { Audio } from "expo-av";
 
 const Inicio = () => {
+  const [sound, setSound] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/miguel.mp3")
+    );
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+    setIsPlaying(true);
+  }
+
+  async function pauseSound() {
+    if (sound && isPlaying) {
+      console.log("Pausing Sound");
+      await sound.pauseAsync();
+      setIsPlaying(false);
+    }
+  }
+
+  async function stopSound() {
+    if (sound) {
+      console.log("Stopping Sound");
+      await sound.stopAsync();
+      setIsPlaying(false);
+    }
+  }
+
+  async function resumeSound() {
+    if (sound && !isPlaying) {
+      console.log("Resuming Sound");
+      await sound.playAsync();
+      setIsPlaying(true);
+    }
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+          setIsPlaying(false);
+        }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Phishing</Text>
       <Image
         resizeMode="stretch"
         style={styles.image}
-        source={{
-          uri: "https://www.valoradata.com/files/uploads/2019/09/phishing.jpg",
-        }}
+        source={require("../assets/ProSecure.png")}
       />
       <View style={styles.definitionContainer}>
-        <TouchableOpacity>
-          <View style={styles.Boton}>
-            <Text style={styles.Boton1}>Play</Text>
-          </View>
+        <TouchableOpacity onPress={playSound}>
+          <Text>Play Sound</Text>
         </TouchableOpacity>
-        <Text style={styles.definitionTitle}>Definicion</Text>
+        <TouchableOpacity onPress={pauseSound} disabled={!isPlaying}>
+          <Text>Pause Sound</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={resumeSound} disabled={isPlaying}>
+          <Text>Resume Sound</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={stopSound}>
+          <Text>Stop Sound</Text>
+        </TouchableOpacity>
+        <Text style={styles.definitionTitle}>Definición</Text>
         <Text style={styles.definitionText}>
           Técnica de ingeniería social en la que los atacantes se hacen pasar
           por entidades confiables para engañar a las víctimas y obtener
@@ -33,44 +94,24 @@ const Inicio = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colores.fondo,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-
   title: {
     fontSize: 25,
     fontWeight: "bold",
     marginTop: 50,
   },
-
-  Boton: {
-    backgroundColor: Colores.fondo,
-    width: 75,
-    height: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    marginLeft: 20,
-    borderRadius: 50,
-    margin: 25,
-  },
-
-  Boton1: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-
   image: {
     marginTop: 15,
     width: 390,
     height: 280,
     borderRadius: 15,
-    position: "static",
   },
-
   definitionContainer: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#fff",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
     overflow: "hidden",
@@ -79,19 +120,16 @@ const styles = StyleSheet.create({
     marginTop: 25,
     marginBottom: 25,
   },
-
   definitionTitle: {
     fontSize: 25,
     fontWeight: "bold",
     marginLeft: 10,
     marginTop: 10,
   },
-
   definitionText: {
-    marginTop: 50,
+    marginTop: 20,
     textAlign: "center",
-    fontSize: 20,
-    fontWeight: "bold",
+    fontSize: 16,
   },
 });
 
