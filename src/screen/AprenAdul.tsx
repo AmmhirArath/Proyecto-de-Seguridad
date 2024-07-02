194ea70a2a227275b66973313dc4b8b140b0efb4
ADRAXLIFE <112525031+ADRAXLIFE@users.noreply.github.com>
2024-07-01 19:22:44 -0500

    menu adul
    arreglado info

--- src/screen/AprenAdul.tsx
+++ src/screen/AprenAdul.tsx
@@ -1,98 +1,198 @@
-import * as React from "react";
-import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
-import Colores from "../constants/Colores";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { Audio, Video } from "expo-av";
+import { SafeAreaView } from "react-native-safe-area-context";
+import PagerView from "react-native-pager-view";
+import Background from "./BackgroundGradient";
 
-const Inicio = () => {
+const AprenAdul = () => {
+  const [sound, setSound] = useState<any>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  useEffect(() => {
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, [sound]);
+
+  const playSound = async () => {
+    if (!sound) {
+      const { sound: audioSound } = await Audio.Sound.createAsync(
+        require("../assets/audio/miguel.mp3")
+      );
+      setSound(audioSound);
+    }
+
+    await sound.playAsync();
+    setIsPlaying(true);
+  };
+
+  const pauseSound = async () => {
+    if (sound && isPlaying) {
+      await sound.pauseAsync();
+      setIsPlaying(false);
+    }
+  };
+
+  const stopSound = async () => {
+    if (sound) {
+      await sound.stopAsync();
+      setIsPlaying(false);
+    }
+  };
+
+  const resumeSound = async () => {
+    if (sound && !isPlaying) {
+      await sound.playAsync();
+      setIsPlaying(true);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Phishing</Text>
-      <Image
-        resizeMode="stretch"
-        style={styles.image}
-        source={{
-          uri: "https://www.valoradata.com/files/uploads/2019/09/phishing.jpg",
-        }}
-      />
-      <View style={styles.definitionContainer}>
-        <TouchableOpacity>
-          <View style={styles.Boton}>
-            <Text style={styles.Boton1}>Play</Text>
-          </View>
+    <SafeAreaView style={styles.container}>
+      <Background />
+      <Text style={styles.text}>Phishing</Text>
+      <PagerView style={styles.pagerView} initialPage={0}>
+        <View key="1" style={styles.page}>
+          <Video
+            source={require("../assets/videos/miguel2.mp4")}
+            style={styles.video}
+            resizeMode="contain"
+            useNativeControls
+          />
+        </View>
+        <View key="2" style={styles.page}>
+          <Image
+            source={require("../assets/Img/nubeGrande.png")}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        </View>
+      </PagerView>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={playSound} style={styles.button}>
+          <Text style={styles.textos}>Empezar</Text>
         </TouchableOpacity>
-        <Text style={styles.definitionTitle}>Definicion</Text>
-        <Text style={styles.definitionText}>
-          Técnica de ingeniería social en la que los atacantes se hacen pasar
-          por entidades confiables para engañar a las víctimas y obtener
-          información sensible, como contraseñas y datos bancarios.
-        </Text>
+        <TouchableOpacity
+          onPress={pauseSound}
+          style={[styles.button, !isPlaying && styles.disabledButton]}
+        >
+          <Text style={styles.textos}>Pausar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={resumeSound}
+          style={[styles.button, isPlaying && styles.disabledButton]}
+        >
+          <Text style={styles.textos}>Reanudar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={stopSound} style={styles.button}>
+          <Text style={styles.textos}>Terminar</Text>
+        </TouchableOpacity>
       </View>
-    </View>
+
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <Text style={styles.text}>Definición</Text>
+        <Text style={styles.paragraph}>
+          Phishing es un término informático que distingue a un conjunto de
+          técnicas que persiguen el engaño a una víctima ganándose su confianza
+          haciéndose pasar por una persona, empresa o servicio de confianza
+          (suplantación de identidad de tercero de confianza), para manipularla
+          y hacer que realice acciones que no debería realizar (por ejemplo,
+          revelar información confidencial o hacer click en un enlace).1​2​ Para
+          realizar el engaño, habitualmente se hace uso de la ingeniería social
+          explotando los instintos sociales de la gente,3​ como es de ayudar o
+          ser eficiente. También mediante la adulación de la víctima, explotando
+          su intrínseca vanidad o necesidad de ser reconocido, baja autoestima,
+          o una persona que busca trabajo. Por ejemplo, enviando correos
+          electrónicos o mostrando publicidades a la víctima diciéndole que ha
+          ganado un premio4​ y que siga un enlace para recibirlo, siendo
+          aquellas promesas falsas (un cebo). A veces también se hace uso de
+          procedimientos informáticos que aprovechan vulnerabilidades.
+          Habitualmente el objetivo es robar información, pero otras veces es
+          instalar malware, sabotear sistemas, o robar dinero a través de
+          fraudes.5​ El phising está considerada una de las técnicas de robos de
+          contraseñas más utilizada.6​ A quien practica el phishing se le llama
+          phisher.7​ Dado el creciente número de denuncias de incidentes
+          relacionados con el phishing, se requieren métodos adicionales de
+          protección. Se han realizado intentos con leyes que castigan la
+          práctica y campañas para prevenir a los usuarios con la aplicación de
+          medidas técnicas a los programas.
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colores.fondo,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  title: {
-    fontSize: 25,
-    fontWeight: "bold",
-    marginTop: 50,
-  },
-
-  Boton: {
-    backgroundColor: Colores.fondo,
-    width: 75,
-    height: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    marginLeft: 20,
-    borderRadius: 50,
-    margin: 25,
-  },
-
-  Boton1: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-
-  image: {
-    marginTop: 15,
-    width: 390,
-    height: 280,
-    borderRadius: 15,
-    position: "static",
-  },
-
-  definitionContainer: {
-    flex: 1,
     backgroundColor: "white",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    overflow: "hidden",
-    width: 410,
-    padding: 15,
-    marginTop: 25,
-    marginBottom: 25,
+    justifyContent: "center",
+    alignItems: "center",
   },
-
-  definitionTitle: {
-    fontSize: 25,
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+  pagerView: {
+    width: "100%",
+    height: "30%", // Ajusta según tus necesidades
+  },
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+  text: {
+    fontSize: 30,
     fontWeight: "bold",
-    marginLeft: 10,
-    marginTop: 10,
+    color: "white",
+    marginTop: 20,
   },
-
-  definitionText: {
-    marginTop: 50,
-    textAlign: "center",
+  scrollContent: {
+    paddingHorizontal: 20,
+    paddingBottom: 40,
+  },
+  paragraph: {
     fontSize: 20,
-    fontWeight: "bold",
+    marginBottom: 20,
+    color: "white",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    marginVertical: 25,
+  },
+  button: {
+    backgroundColor: "white",
+    height: 40,
+    width: 80,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textos: {
+    fontSize: 14,
+    color: "black",
+  },
+  disabledButton: {
+    opacity: 0.5,
   },
 });
 
-export default Inicio;
+export default AprenAdul;
--- src/screen/HomeAdult.tsx
+++ src/screen/HomeAdult.tsx
@@ -1,38 +1,51 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import Colores from "../constants/Colores";
 import { useNavigation } from "@react-navigation/native";
 import Background from "./BackgroundGradient";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Bienvenidos = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Background />
-      <Text style={styles.welcomeText}>Bienvenido</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("AprenderAdult")}
-      >
-        <Text style={styles.buttonText}>Informacion</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Juegos")}
-      >
-        <Text style={styles.buttonText}>Juegos</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Chatbot")}
-      >
-        <Text style={styles.buttonText}>Chatbot</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Salir</Text>
-      </TouchableOpacity>
-    </View>
+      <Text style={styles.welcomeText}>Bienvenido </Text>
+      <Image
+        source={require("../assets/Img/Logosptm.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <View style={styles.containerBotton}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("AprenderAdult")}
+        >
+          <Text style={styles.buttonText}>Informacion</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Juegos")}
+        >
+          <Text style={styles.buttonText}>Juegos</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Chatbot")}
+        >
+          <Text style={styles.buttonText}>Chatbot</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Salir</Text>
+        </TouchableOpacity>
+      </View>
+      <Image
+        source={require("../assets/Img/nubeGrande.png")}
+        style={styles.image2}
+        resizeMode="contain"
+      />
+    </SafeAreaView>
   );
 };
 
@@ -43,11 +56,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   welcomeText: {
-    fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 40,
     color: Colores.blanco,
+    fontSize: 25,
+    marginVertical: 50,
   },
+  image: {
+    marginVertical: 20,
+  },
+  image2: {
+    height: 250,
+    position: "relative",
+  },
+
   button: {
     backgroundColor: "white",
     paddingVertical: 10,
@@ -55,10 +76,14 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginVertical: 10,
   },
+  containerBotton: {
+    flex: 1,
+  },
   buttonText: {
     fontSize: 18,
     fontWeight: "bold",
     color: "black",
+    textAlign: "center",
   },
 });
 
